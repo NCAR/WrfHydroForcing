d20ff2d3ef2a4b482b515162261d5c3f2cced86c
logankarsten <karsten@ucar.edu>
2019-01-02 10:35:31 -0700

    Initial commit for establishing FE repository.

---
+++ genForcing.py
@@ -0,0 +1,9 @@
+def main(argv):
+    """ Main program to process LDASIN forcing files for WRF-Hydro.
+        The main execution of the program requires the user to compose
+        a forcing configuration file that is parsed by this workflow.
+        Please see documentation for further instructions."""
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
