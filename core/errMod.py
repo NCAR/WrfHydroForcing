5fb5e5d056ed0b3e32a08e0718bb73a2bb6f463e
logankarsten <karsten@ucar.edu>
2019-09-17 16:26:18 -0600

    Adding global atts and commenting out NDV check for supplemental precip as this will be possible before layering with MRMS.

--- core/errMod.py
+++ core/errMod.py
@@ -309,11 +309,11 @@ def check_supp_pcp_bounds(ConfigOptions, supplemental_precip, MpiConfig):
     # First check to see if we have any data that is not missing.
     indCheck = np.where(supplemental_precip.regridded_precip2 != ConfigOptions.globalNdv)
 
-    if len(indCheck[0]) == 0:
-        ConfigOptions.errMsg = "No valid supplemental precip found in " + supplemental_precip.file_in2
-        log_critical(ConfigOptions, MpiConfig)
-        indCheck = None
-        return
+    #if len(indCheck[0]) == 0:
+    #    ConfigOptions.errMsg = "No valid supplemental precip found in " + supplemental_precip.file_in2
+    #    log_critical(ConfigOptions, MpiConfig)
+    #    indCheck = None
+    #    return
 
     # Check to see if any pixel cells are below the minimum value.
     indCheck = np.where((supplemental_precip.regridded_precip2 != ConfigOptions.globalNdv) &
