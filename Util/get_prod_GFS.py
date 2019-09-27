827b1f4ded31117544e83cf2492840ebbcbcfe79
logankarsten <karsten@ucar.edu>
2019-09-27 00:31:30 -0600

    Increasing period of CFS data in utility pull program.

--- Util/get_prod_GFS.py
+++ Util/get_prod_GFS.py
@@ -44,7 +44,7 @@ def msgUser(msgContent,msgFlag):
 
 outDir = "/glade/p/cisl/nwc/nwm_forcings/Forcing_Inputs/GFS_13km_Global"
 tmpDir = "/glade/scratch/karsten"
-lookBackHours = 48 # How many hours to look for data.....
+lookBackHours = 60 # How many hours to look for data.....
 cleanBackHours = 240 # Period between this time and the beginning of the lookback period to cleanout old data.  
 lagBackHours = 6 # Wait at least this long back before searching for files. 
 dNowUTC = datetime.datetime.utcnow()
