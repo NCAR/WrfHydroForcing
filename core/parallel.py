dc21f9eea518107679ff8b8b1c77f87c5fa5937d
Donald W Johnson <Donald.W.Johnson@noaa.gov>
2020-02-06 18:07:20 +0000

    Updated scaterv logic to not use cache. Updated scatter_array to be wrapper around differnt scatter implementations.

--- core/parallel.py
+++ core/parallel.py
@@ -17,7 +17,7 @@ class MpiConfig:
         """
         self.comm = None
         self.rank = None
-        self.size = None
+        self.size = None 
 
     def initialize_comm(self, config_options):
         """
@@ -63,15 +63,73 @@ class MpiConfig:
             return None
         return tmp_dict['varTmp']
 
-    def scatter_array(self, geoMeta, src_array, config_options):
+    def scatter_array(self, geoMeta, array_broadcast, ConfigOptions):
+        #return scatter_array_logan(self, geoMeta, array_broadcast, ConfigOptions)
+        return scatter_array_scatterv_no_cache(self, geoMeta, arrayOptions, ConfigOptions)
+
+    def scatter_array_logan(self, geoMeta, array_broadcast, ConfigOptions):
+        """
+        Generic function for calling scatter functons based on
+        the input dataset type.
+        :param geoMeta:
+        :param array_broadcast:
+        :param ConfigOptions:
+        :return:
+        """
+        # Determine which type of input array we have based on the
+        # type of numpy array.
+        data_type_flag = -1
+        if self.rank == 0:
+            if array_broadcast.dtype == np.float32:
+                data_type_flag = 1
+            if array_broadcast.dtype == np.float64:
+                data_type_flag = 2
+
+        # Broadcast the numpy datatype to the other processors.
+        if self.rank == 0:
+            tmpDict = {'varTmp': data_type_flag}
+        else:
+            tmpDict = None
+        try:
+            tmpDict = self.comm.bcast(tmpDict, root=0)
+        except:
+            ConfigOptions.errMsg = "Unable to broadcast numpy datatype value from rank 0"
+            err_handler.log_critical(ConfigOptions, MpiConfig)
+            return None
+        data_type_flag = tmpDict['varTmp']
+
+        # Broadcast the global array to the child processors, then
+        if self.rank == 0:
+            arrayGlobalTmp = array_broadcast
+        else:
+            if data_type_flag == 1:
+                arrayGlobalTmp = np.empty([geoMeta.ny_global,
+                                           geoMeta.nx_global],
+                                          np.float32)
+            if data_type_flag == 2:
+                arrayGlobalTmp = np.empty([geoMeta.ny_global,
+                                           geoMeta.nx_global],
+                                          np.float64)
+        try:
+            self.comm.Bcast(arrayGlobalTmp, root=0)
+        except:
+            ConfigOptions.errMsg = "Unable to broadcast a global numpy array from rank 0"
+            err_handler.log_critical(ConfigOptions, MpiConfig)
+            return None
+        arraySub = arrayGlobalTmp[geoMeta.y_lower_bound:geoMeta.y_upper_bound,
+                   geoMeta.x_lower_bound:geoMeta.x_upper_bound]
+        return arraySub
+
+    def scatter_array_scatterv_no_cache(self,geoMeta,src_array,ConfigOptions):
         """
             Generic function for calling scatter functons based on
             the input dataset type.
             :param geoMeta:
-            :param src_array:
+            :param array_broadcast:
             :param ConfigOptions:
             :return:
         """
+
 
         # Determine which type of input array we have based on the
         # type of numpy array.
@@ -84,59 +142,36 @@ class MpiConfig:
 
         # Broadcast the numpy datatype to the other processors.
         if self.rank == 0:
-            tmp_dict = {'varTmp': data_type_flag}
+            tmpDict = {'varTmp': data_type_flag}
         else:
-            tmp_dict = None
+            tmpDict = None
         try:
-            tmp_dict = self.comm.bcast(tmp_dict, root=0)
-        except MPI.Exception:
-            config_options.errMsg = "Unable to broadcast numpy datatype value from rank 0"
-            err_handler.log_critical(config_options, MpiConfig)
+            tmpDict = self.comm.bcast(tmpDict, root=0)
+        except:
+            ConfigOptions.errMsg = "Unable to broadcast numpy datatype value from rank 0"
+            err_handler.log_critical(ConfigOptions, MpiConfig)
             return None
-        data_type_flag = tmp_dict['varTmp']
+        data_type_flag = tmpDict['varTmp']
+
 
         # gather buffer offsets and bounds to rank 0
-        if geoMeta.has_cache:
-            x_lower = geoMeta.global_x_lower
-            y_lower = geoMeta.global_y_lower
-            x_upper = geoMeta.global_x_upper
-            y_upper = geoMeta.global_y_upper
-        else:
-            try:
-                x_lower = np.asarray(self.comm.allgather(np.int32(geoMeta.x_lower_bound)))
-            except:
-               config_options.errMs = "Failed all gathering buffer x lower at rank " + str(self.comm.rank)
-               err_handler.log_critical(config_options,MpiConfig)
-               return None
+        bounds = np.array(
+            [np.int32(geoMeta.x_lower_bound), np.int32(geoMeta.y_lower_bound),
+             np.int32(geoMeta.x_upper_bound), np.int32(geoMeta.y_upper_bound)])
+        global_bounds = np.zeros((self.comm.size*4),np.int32)
 
-            try:
-                y_lower = np.asarray(self.comm.allgather(np.int32(geoMeta.y_lower_bound)))
-            except:
-                config_options.errMsg = "Failed all gathering buffer y lower at rank " + str(self.comm.rank)
-                err_handler.log_critical(config_options,MpiConfig)
-                return None
+        try:
+            self.comm.Allgather([bounds, MPI.INTEGER], [global_bounds, MPI.INTEGER])
+        except:
+            ConfigOptions.errMsg = "Failed all gathering global bounds at rank" + str(self.comm.rank)
+            err_handler.log_critical(ConfigOptions, MpiConfig)
+            return None
 
-            try:
-                x_upper = np.asarray(self.comm.allgather(np.int32(geoMeta.x_upper_bound)))
-            except:
-                config_options.errMsg = "Failed all gathering buffer x upper at rank " + str(self.comm.rank)
-                err_handler.log_critical(config_options,MpiConfig)
-                return None
-
-            try:
-                y_upper = np.asarray(self.comm.allgather(np.int32(geoMeta.y_upper_bound)))
-            except:
-                config_options.errMsg = "Failed all gathering buffer x upper at rank " + str(self.comm.rank)
-                err_handler.log_critical(config_options,MpiConfig)
-                return None
-
-            # all ranks records global intervals all ranks mark existance of cache
-            geoMeta.global_x_lower = x_lower
-            geoMeta.global_y_lower = y_lower
-            geoMeta.global_x_upper = x_upper
-            geoMeta.global_y_upper = y_upper
-            geoMeta.has_cache = True
-
+        # create slices for x and y bounds arrays
+        x_lower = global_bounds[0:(self.comm.size * 4) + 0:4]
+        y_lower = global_bounds[1:(self.comm.size * 4) + 1:4]
+        x_upper = global_bounds[2:(self.comm.size * 4) + 2:4]
+        y_upper = global_bounds[3:(self.comm.size * 4) + 3:4]
 
         # we know know the local region for each rank
         if self.rank == 0:
@@ -149,14 +184,13 @@ class MpiConfig:
             sendbuf = None
 
         # generate counts
-        counts = [ (y_upper[i] -y_lower[i]) *(x_upper[i]- x_lower[i])
+        counts = [ (y_upper[i] - y_lower[i]) * (x_upper[i] - x_lower[i])
                    for i in range(0,self.comm.size)]
 
-        #generate offsets
+        #generate offsets:
         offsets = [0]
         for i in range(1, len(counts)):
             offsets.append(offsets[i - 1] + counts[i])
-        i = None
 
         #create the recvbuffer
         if data_type_flag == 1:
@@ -170,17 +204,10 @@ class MpiConfig:
         try:
             self.comm.Scatterv( [sendbuf, counts, offsets, data_type], recvbuf, root=0)
         except:
-            config_options.errMsg = "Failed to scatter from rank 0"
-            err_handler.log_critical(config_options, MpiConfig)
+            ConfigOptions.errMsg = "Failed Scatterv from rank 0"
+            err_handler.log_critical(ConfigOptions, MpiConfig)
             return None
 
-        try:
-            subarray = np.reshape(recvbuf,[y_upper[self.rank] -y_lower[self.rank],x_upper[self.rank]- x_lower[self.rank]])
-            return subarray
-        except:
-            config_options.errMsg = "Reshape failed for dimensions [" + \
-                                  str(y_upper[self.rank]-y_lower[self.rank]) + \
-                                  ","+str(x_upper[self.rank]-x_upper[self.rank]) + \
-                                  "] at rank:" +str(self.rank)
-            err_handler.log_critical(config_options, MpiConfig)
-            return None
+        subarray = np.reshape(recvbuf,[y_upper[self.rank] -y_lower[self.rank],x_upper[self.rank]- x_lower[self.rank]])
+        return subarray
+
