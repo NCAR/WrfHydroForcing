b883e1434ca95001fa89d6677ef95f77d9c5ec69
Bill Petzke <bpetzke@ucar.edu>
2021-07-13 16:21:58 -0600

    Update config MRMS directory. Modified clean_ExtAnA.sh code

--- Test/cleanup_ExtAnA.sh
+++ Test/cleanup_ExtAnA.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf /glade/scratch/bpetzke/ForcingEngine/Test/ExtAnA/Alaska/2021070121/; rm -f /glade/scratch/bpetzke/ForcingEngine/Test/ExtAnA/Alaska/grib2.tbl; rm -f /glade/scratch/bpetzke/ForcingEngine/Test/ExtAnA/Alaska/LOG*; rm -f EXT*
+rm -rf /glade/scratch/bpetzke/ForcingEngine/Test/ExtAnA/Alaska/2021??????/; rm -f /glade/scratch/bpetzke/ForcingEngine/Test/ExtAnA/Alaska/grib2.tbl; rm -f /glade/scratch/bpetzke/ForcingEngine/Test/ExtAnA/Alaska/LOG*; rm -f EXT*
