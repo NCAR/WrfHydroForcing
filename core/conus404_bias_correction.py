ffa0f73a5c3ebe8beaca38fdb0fd1d1b37688c67
Ryan Cabell <rcabell@ucar.edu>
2022-10-13 16:20:37 -0600

    Fix inverted non-leap year offset

--- core/conus404_bias_correction.py
+++ core/conus404_bias_correction.py
@@ -18,31 +18,41 @@ def conus404_daymet_precip_bias_correction(input_forcings, config_options, mpi_c
                                  "precipitation", "precip", lambda x, y: x * (1 + (y/100)))
 
 
+def jday_for_date(in_date):
+    # determine day-of-year
+    jday = in_date.timetuple().tm_yday
+
+    # adjust as necessary for leap year
+    adjusted = False
+    leap_year = datetime(in_date.year, 12, 31).timetuple().tm_yday == 366
+    if (not leap_year) and jday >= 60:
+        adjusted = True
+        jday += 1
+
+    return jday, adjusted
+
+
 def apply_daymet_bias_correction(input_forcings, config_options, mpi_config, force_num, geo_meta, description, variable, operator):
     '''
     Apply CONUS404 Daymet-based temperature bias correction to native WRF-Hydro CONUS grid
     '''
 
     if mpi_config.rank == 0:
-        # determine day-of-year
-        jday = input_forcings.fcst_date1.timetuple().tm_yday
-
-        # adjust as necessary for leap year
-        adjusted = False
-        leap_year = datetime(input_forcings.fcst_date1.year, 12, 31).timetuple().tm_yday == 366
-        if (not leap_year) and jday >= 60:
-            adjusted = True
-            jday -= 1
+        jday, adjusted = jday_for_date(input_forcings.fcst_date1)
 
         if mpi_config.rank == 0:
             config_options.statusMsg = f"Performing CONUS404/Daymet {description} bias corrrection " \
-                                       f"for day {jday + 1 if adjusted else jday} of year {input_forcings.fcst_date1.year}"
+                                       f"for parameter day {jday} {f'(actual day {jday-1}) ' if adjusted else ''}" \
+                                       f"of year {input_forcings.fcst_date1.year}"
             err_handler.log_msg(config_options, mpi_config)
 
         # open appropriate offset file
         try:
             offset_file_path = path.join(config_options.dScaleParamDirs[0], 'bias_corr_1km',
                                          f'CONUS404_Daymet_1km_{variable}_bias_corr_doy_{jday:03}.nc')
+            if mpi_config.rank == 0:
+                config_options.statusMsg = f"Loading bias corrections from file: {offset_file_path}"
+                err_handler.log_msg(config_options, mpi_config)
 
             # read entire grid to root processor
             offset_ds = Dataset(offset_file_path)
