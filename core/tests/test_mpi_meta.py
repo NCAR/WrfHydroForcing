66071e0564c2dbff60c4da0c47ebbcd177cba723
Andrew Gaydos <gaydos@ucar.edu>
2023-09-08 14:47:31 -0600

    Added tests and assertions for scattering an array using ESMF and mpi

--- core/tests/test_mpi_meta.py
+++ core/tests/test_mpi_meta.py
@@ -56,9 +56,27 @@ def test_broadcast_parameter(config_options, mpi_meta):
 
 @pytest.mark.mpi
 def test_scatter_array(config_options, mpi_meta, geo_meta):
-    test_array = np.zeros([1,390,590], dtype=np.float64)
 
-    #arr = mpi_meta.scatter_array(geo_meta,test_array,config_options)
+    target_data = None
+    if mpi_meta.rank == 0:
+        target_data = np.random.randn(geo_meta.ny_global,geo_meta.nx_global)
+
+    # get the sum of the full grid.
+    value = np.sum(target_data) if mpi_meta.rank == 0 else None
+    value = mpi_meta.broadcast_parameter(value, config_options, param_type=float)
+    
+    target_data = mpi_meta.scatter_array(geo_meta,target_data,config_options)
+
+    # None of the sub grids' sum should equal the sum of the full grid
+    # unless there's just a single process
+    assert np.sum(target_data) != value or mpi_meta.size == 1
+
+    # The x dimension size should be the same as the full grid
+    assert target_data.shape[1] == geo_meta.nx_global
+
+    # The length of the y dimension of the sub grid should equal (full grid.y / num_processes),
+    # account for rounding when scattering the array
+    assert abs((target_data.shape[0] * mpi_meta.size) - geo_meta.ny_global) < mpi_meta.size
 
 
-    assert True
+ 
