63e8c710a5ba1d88174c92533668b9dc96f1be1b
Andrew Gaydos <gaydos@ucar.edu>
2023-09-11 13:28:16 -0600

    Added geoMod tests. Added mpi scatter_array tests. Modified run_tests.py to test various MPI n_procs (none, 1, 4, and 8) for consistency

--- core/tests/test_mpi_meta.py
+++ core/tests/test_mpi_meta.py
@@ -4,29 +4,9 @@ from core.config import ConfigOptions
 from core.geoMod import GeoMetaWrfHydro
 import numpy as np
 
-@pytest.fixture
-def config_options():
-    config_path = 'yaml/configOptions_valid.yaml'
-    return ConfigOptions(config_path)
+from fixtures import *
 
-@pytest.fixture
-def geo_meta(config_options, mpi_meta):
-    config_options.read_config()
 
-    WrfHydroGeoMeta = GeoMetaWrfHydro()
-    WrfHydroGeoMeta.initialize_destination_geo(config_options, mpi_meta)
-    WrfHydroGeoMeta.initialize_geospatial_metadata(config_options, mpi_meta)
-
-    return WrfHydroGeoMeta
-
-@pytest.fixture
-def mpi_meta(config_options):
-    config_options.read_config()
-
-    mpi_meta = MpiConfig()
-    mpi_meta.initialize_comm(config_options)
-
-    return mpi_meta
 
 @pytest.mark.mpi
 def test_create_mpi_config(config_options, mpi_meta):
