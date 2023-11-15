1d5d0057dc5899fbe049ae367af4e68310cf267d
amazroo <mazrooei@ucar.edu>
2023-11-15 16:53:43 -0700

    Add NBM PTYPE

--- core/forcingInputMod.py
+++ core/forcingInputMod.py
@@ -230,7 +230,7 @@ class input_forcings:
                  'DLWRF', 'PRES'],
             20: ['U2D', 'V2D', 'LWDOWN', 'RAINRATE', 'T2D',
                  'Q2D', 'PSFC', 'SWDOWN'],
-            21: ['TMP', 'APCP']
+            21: ['TMP', 'APCP', 'PTYPE']
         }
         self.grib_vars = grib_vars_in[self.keyValue]
 
@@ -279,7 +279,7 @@ class input_forcings:
                 '10 m above ground', '10 m above ground',
                 'surface', 'surface', 'surface', 'surface'],
             20: None,
-            21: ['2 m above ground', 'surface']
+            21: ['2 m above ground', 'surface', 'surface']
         }
         self.grib_levels = grib_levels_in[self.keyValue]
 
@@ -344,7 +344,7 @@ class input_forcings:
                 'PRES_surface'],
             20: ['U2D', 'V2D', 'LWDOWN', 'RAINRATE', 'T2D',
                  'Q2D', 'PSFC', 'SWDOWN'],
-            21: ['TMP_2maboveground', 'APCP_surface']
+            21: ['TMP_2maboveground', 'APCP_surface', 'PTYPE_surface']
         }
         self.netcdf_var_names = netcdf_variables[self.keyValue]
 
@@ -396,7 +396,7 @@ class input_forcings:
             18: [4, 5, 0, 1, 3, 6],
             19: [4,5,0,1,3,7,2,6],
             20: [0,1,2,3,4,5,6,7],
-            21: [4, 3]
+            21: [4, 3, 8] # 8 is the LQFRAC liquid water fraction
         }
         self.input_map_output = input_map_to_outputs[self.keyValue]
 
